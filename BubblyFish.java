2d42773d400ca3ff47e0250914ff0bb94e62f2ee
faustinabui <candylolipop173@gmail.com>
2024-05-31 21:20:35 -0700

    Add files via upload

---
+++ BubblyFish.java
@@ -0,0 +1,31 @@
+/**
+* Using output and escape sequences, create a bubbly fish from scratch
+*/
+
+/**
+*  The BubblyFish Program.
+*  @author Faustina Bui
+*  @version June 16, 2023
+*/
+
+public class BubblyFish
+{
+   public static void main(String[] args)
+   {
+       System.out.println("o                 o");
+       System.out.println("                 o   ");
+       System.out.println("          o   ______        o");
+       System.out.println("            _/  (   \\_");
+       System.out.println(" _        _/  (       \\_    O");
+       System.out.println("| \\_   _/   (   (    0  \\");
+       System.out.println("|== \\_/   (   (          |");
+       System.out.println("|=== _  (   (   (        |");
+       System.out.println("|==_/ \\_  (   (          |");
+       System.out.println("|_/     \\_  (   (     \\__/");
+       System.out.println("           \\_  (       _/");
+       System.out.println("              |  |___/");
+       System.out.println("  \"          /__/");
+   }
+}
+
+
